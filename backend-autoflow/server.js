7537e65068b7721a2391b1b3e129fd8e6b55d9f4
adminlocal <sarra.bngharbia@gmail.com>
2025-03-10 21:29:23 +0100

    premier commit

---
+++ backend-autoflow/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+dotenv.config();
+const app = express();
+app.get("/products", (req, res) => {});
+   
+
+
+app.listen(5000, () => {
+    connectDB();
+    console.log("Server started at http://localhost:5000");
+
+});
+// 1pA39c9rUl7I0SR8 
